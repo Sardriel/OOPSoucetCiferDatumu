8c50c61745950b5f1fdb9ba37d52e5cd3b20aadb
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-13 17:01:46 +0100

    Add project files.

---
+++ OOPSoucetCiferDatumu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace OOPSoucetCiferDatumu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime datumCas = DateTime.Today;
+            Console.WriteLine("Vypisuji součty cifer v datu pro následujících 7 dní");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("{0} - {1}", datumCas.AddDays(i).ToShortDateString(), datumCas.AddDays(i).Day + datumCas.Month + datumCas.Year);
+            }
+
+
+            
+        }
+    }
+}
